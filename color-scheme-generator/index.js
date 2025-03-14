7e8fb84d91da8d7cf4b57ecb06b8a70ce8dca45d
Neil <iccir919@gmail.com>
2025-03-13 20:58:34 -0700

    color-scheme-generator: complete basic requirements

--- color-scheme-generator/index.js
+++ color-scheme-generator/index.js
@@ -1,1 +1,43 @@
-console.log("Testing 1, 2, 3")
+
+document.getElementById("color-scheme-form").addEventListener("submit", function(e) {
+    e.preventDefault()
+
+    let hexColor = document.getElementById("seed-color").value.substring(1)
+    let mode = document.getElementById("color-scheme-mode").value
+
+    getColorScheme(hexColor, mode)
+})
+
+function getColorScheme(hexColor, mode) {
+    fetch(`https://www.thecolorapi.com/scheme?hex=${hexColor}&mode=${mode}`)
+        .then(response => response.json())
+        .then(data => renderColorScheme(data.colors))
+}
+
+function renderColorScheme(colorsArr) {
+    colorsArr.forEach((color, idx) => {
+        const display = document.getElementById(`display-color-${idx + 1}`)
+        const text = document.getElementById(`text-value-color-${idx + 1}`)
+
+        display.style.backgroundColor = color.hex.value
+        text.textContent = color.hex.value
+    })
+}
+
+
+function getRandomHexColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+  
+function intialize() {
+    const randomHexColor = getRandomHexColor()
+    document.getElementById("seed-color").value = `#${randomHexColor}`
+    getColorScheme(randomHexColor, 'monochrome')
+}
+
+intialize()
