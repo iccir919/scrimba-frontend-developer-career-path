777690b652b43f4e03a0d8c3a51c967f1819e9b4
Neil <iccir919@gmail.com>
2025-03-26 09:06:57 -0700

    movie-watchlist implement API call for movie search

--- movie-watchlist/script.js
+++ movie-watchlist/script.js
@@ -1,1 +1,32 @@
-console.log("Testing 1, 2, 3")
+
+let pageNumber = 1
+const searchInput = document.getElementById("search-input")
+const searchBtn = document.getElementById("search-btn")
+const searchResultContainer = document.getElementById("search-results-container")
+
+
+searchBtn.addEventListener("click", async (e) => {
+    e.preventDefault()
+
+    const movie = searchInput.value
+
+    const params = new URLSearchParams()
+    params.append("apikey", "3868c4dd")
+    params.append("s", movie)
+
+    const response = await fetch(`http://www.omdbapi.com/?${params}`)
+    const searchResult = await response.json()
+
+    pageNumber = 1
+    renderMovieResults(searchResult)
+})
+
+function renderMovieResults(result) {
+    let moviesHtml = ""
+    result.Search.forEach(movie => moviesHtml += `
+        <div class="movie-container">
+            <h2>${movie["Title"]}</h2>
+        </div>    
+    `)
+    searchResultContainer.innerHTML = moviesHtml
+}
