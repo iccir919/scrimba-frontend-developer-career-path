792fc1e58e42e8b1d8525d8113bfb6e8f68d1a97
Neil <iccir919@gmail.com>
2025-03-28 05:27:22 -0700

    movie-watchlist implement basic render of movies with details

--- movie-watchlist/script.js
+++ movie-watchlist/script.js
@@ -1,8 +1,8 @@
 
-let pageNumber = 1
 const searchInput = document.getElementById("search-input")
 const searchBtn = document.getElementById("search-btn")
 const searchResultContainer = document.getElementById("search-results-container")
+const omdbApiUrl = "http://www.omdbapi.com/?apikey=3868c4dd&"
 
 
 searchBtn.addEventListener("click", async (e) => {
@@ -10,23 +10,48 @@ searchBtn.addEventListener("click", async (e) => {
 
     const movie = searchInput.value
 
-    const params = new URLSearchParams()
-    params.append("apikey", "3868c4dd")
-    params.append("s", movie)
+    fetch(omdbApiUrl + "s=" + encodeURIComponent(movie))
+        .then(response => response.json())
+        .then(result => renderMovieResults(result["Search"]))
 
-    const response = await fetch(`http://www.omdbapi.com/?${params}`)
-    const searchResult = await response.json()
-
-    pageNumber = 1
-    renderMovieResults(searchResult)
 })
 
-function renderMovieResults(result) {
-    let moviesHtml = ""
-    result.Search.forEach(movie => moviesHtml += `
-        <div class="movie-container">
-            <h2>${movie["Title"]}</h2>
-        </div>    
-    `)
-    searchResultContainer.innerHTML = moviesHtml
+function renderMovieResults(movies) {
+    let movieResultsHtml = ""
+    movies.forEach( (movie) => {
+        console.log(movie)
+        movieResultsHtml += `
+            <div class="movie-container" id="${movie["imdbID"]}"></div>
+        `
+    })
+    searchResultContainer.innerHTML = movieResultsHtml
+    getMovieDetails(movies)
+}
+
+function getMovieDetails(movies) {
+    movies.forEach(movie => {
+        fetch(omdbApiUrl + "i=" + movie["imdbID"])
+            .then(response => response.json())
+            .then(movie => {
+                document.getElementById(movie["imdbID"]).innerHTML = `
+                    <img 
+                        class="movie-poster"
+                        src="${movie["Poster"]}" 
+                        alt="Movie poster for ${movie["Title"]}" 
+                    />
+                    <div class="movie-details-container">
+                        <div class="movie-details-top-row">
+                            <h2 class="movie-title">${movie["Title"]}</h2>
+                            <p class="movie-detail">IMDB rating: ${movie["imdbRating"]}</p>
+                        </div>
+                        <div class="movie-details-bottom-row">
+                            <p class="movie-detail movie-runtime">${movie["Runtime"]}</p>
+                            <p class="movie-detail movie-category">${movie["Genre"]}</p>
+                            <button>Watchlist</button>
+                        </div>
+                    </div>
+                `
+            })
+
+    })
 }
