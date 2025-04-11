68620cee020cbd9cb1a4963eb52fc1b1b147fd1e
Neil <iccir919@gmail.com>
2025-04-10 22:52:12 -0700

    dashboard add fitbit authentication

--- dashboard/connect-fitbit.js
+++ dashboard/connect-fitbit.js
@@ -1,8 +1,13 @@
-const urlParams = new URLSearchParams(window.location.hash.substring(1));
-const accessToken = urlParams.get('access_token');
+const urlParams = new URLSearchParams(window.location.hash.substring(1))
+const fitbitAccessToken = urlParams.get('access_token')
+const statusText = document.getElementById("connect-fitbit-status")
 
-if (accessToken) {
-  console.log('Access Token:', accessToken);
+if (fitbitAccessToken) {
+    localStorage.setItem('fitbitAccessToken', fitbitAccessToken)
+    statusText.innerHTML = "<h2>Fitbit connected!</h2>"
+
 } else {
-  console.log('Access token not found in URL');
+  statusText.innerHTML = `<h2>
+    Fitbit not connected. Please <a href="connect.html">try again</a>.
+  </h2>`
 }
--- dashboard/connect.js
+++ dashboard/connect.js
@@ -1,3 +1,12 @@
+const fitbitAccessToken = localStorage.getItem("fitbitAccessToken")
+const fitbitStatusText = document.getElementById("fitbit-status")
+
+if (fitbitAccessToken) {
+    fitbitStatusText.innerText = "Connected!"
+} else {
+    fitbitStatusText.innerText = "Not connected!"
+}
+
 document.getElementById("fitbit-link").href = getFitbitAuthorizeUri()
 
 function getFitbitAuthorizeUri() {
@@ -6,7 +15,7 @@ function getFitbitAuthorizeUri() {
         client_id: "23QDPZ",
         response_type: "token",
         scope: "activity heartrate profile sleep",
-        redirect_uri: "https://neil-scrimba-frontend-career-path.netlify.app/dashboard/connect.html",
+        redirect_uri: "https://neil-scrimba-frontend-career-path.netlify.app/dashboard/connect-fitbit.html",
         expires_in: 31536000
     })
     fitbitAuthorizeUri.search = params
