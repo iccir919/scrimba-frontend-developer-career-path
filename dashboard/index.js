2f519cd669d929033ca7d662609cad0a071b6353
Neil <iccir919@gmail.com>
2025-05-16 09:38:02 -0700

    Display current location

--- dashboard/index.js
+++ dashboard/index.js
@@ -1,11 +1,9 @@
+const curentPositionLatitude = 37.81262421636472
+const currentPositionLongitude = -122.24471522191392
+
 function getRandomBackground() {
-    fetch("https://picsum.photos/1920/1080", {
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
+    fetch("https://picsum.photos/1920/1080")
         .then(res => {
-            console.log(res)
             document.body.style.backgroundImage = `url(${res.url})`
         })
         .catch(err => {
@@ -20,5 +18,19 @@ function getCurrentTime() {
     setInterval(getCurrentTime, 1000)
 }
 
+
+function getPositionProperties() {
+    fetch(`https://api.weather.gov/points/${curentPositionLatitude},${currentPositionLongitude}`)
+        .then(res => res.json())
+        .then(data => {
+            const { city, state } = data.properties.relativeLocation.properties
+            document.getElementById("weather-container").innerHTML += `
+                <h3>${city}, ${state}</h3>
+            `
+            console.log(data)      
+        })
+}
+
 getRandomBackground()
 getCurrentTime()
+getPositionProperties()
