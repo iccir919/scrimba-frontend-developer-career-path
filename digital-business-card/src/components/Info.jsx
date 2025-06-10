09a795782fd4c29fd71b59d23b86baff4940eebb
Neil <iccir919@gmail.com>
2025-06-10 06:09:45 -0700

    digital-business-card begin Info component

--- digital-business-card/src/components/Info.jsx
+++ digital-business-card/src/components/Info.jsx
@@ -12,18 +12,24 @@ export default function Info(props) {
                 src={props.personImgUrl}  
             />
             <div className="person-info-details-container">
-                <h1 className="personName">{firstName} {lastName}</h1>
-                <p className="jobTitle">{jobTitle}</p>
+                <h1 className="person-name">{firstName} {lastName}</h1>
+                <p className="job-title">{jobTitle}</p>
                 <a 
                     href={websiteUrl}
-                    className="wesbiteLink"
+                    className="person-website-link"
                 >
                     {(firstName + " " + lastName).toLowerCase()} website
                 </a>
-            </div>
-            <div className="person-info-buttons-container">
-                <button>Email</button>
-                <button>LinkedIn</button>
+                <div className="person-info-buttons-container">
+                    <button className="btn-light">
+                        <i className="btn-icon fa-solid fa-envelope"></i>
+                        Email
+                    </button>
+                    <button className="btn-dark">
+                        <i className="btn-icon fa-brands fa-linkedin"></i>
+                        LinkedIn
+                    </button>
+                </div>
             </div>
         </header>
     )
