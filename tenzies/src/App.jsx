80bc0ba774ed3536a376df6f61c9d5065bb0e027
Neil <iccir919@gmail.com>
2025-07-11 08:44:41 -0700

    tenzies - implement generateAllNewDice

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,6 +1,13 @@
 import Die from "./components/Die"
 
 export default function App() {
+
+  function generateAllNewDice() {
+    return new Array(10)
+      .fill(0)
+      .map(() => Math.ceil(Math.random() * 6))
+  }
+
   return (
     <main>
       <div className="dice-container">
