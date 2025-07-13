af65146620da0d4e120c21bafd74f4fdc200de70
Neil <iccir919@gmail.com>
2025-07-13 16:49:06 -0700

    tenzies - Add Roll button

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react"
 import Die from "./components/Die"
 
 export default function App() {
+
+  const [dice, setDice] = useState(generateAllNewDice())
 
   function generateAllNewDice() {
     return new Array(10)
@@ -8,20 +11,19 @@ export default function App() {
       .map(() => Math.ceil(Math.random() * 6))
   }
 
+  function rollDice() {
+    setDice(generateAllNewDice())
+  }
+
+  const diceElements = dice.map(num => <Die value={num} /> )
+
   return (
     <main>
       <div className="dice-container">
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
-        <Die value={5} />
-        <Die value={6} />
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
+        {diceElements}
       </div>
+
+      <button className="roll-dice">Roll</button>
     </main>
   )
 }
