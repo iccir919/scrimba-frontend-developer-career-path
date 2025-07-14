a92bd12efadf80b0b5732083ad9a75f4dc75a9a4
Neil <iccir919@gmail.com>
2025-07-14 09:10:20 -0700

    tenzies - implement rollDice function

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -17,7 +17,11 @@ export default function App() {
   }
 
   function rollDice() {
-    setDice(generateAllNewDice())
+    setDice(oldDice => oldDice.map(die => 
+      die.isHeld ? 
+        die : 
+        { ...die, value: Math.ceil(Math.random() * 6) }
+    ))
   }
 
   function hold(id) {
@@ -39,6 +43,8 @@ export default function App() {
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {diceElements}
       </div>
