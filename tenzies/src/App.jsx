1c995f2a8214c0aad157809c8d2769b53342a265
Neil <iccir919@gmail.com>
2025-07-13 23:12:07 -0700

    tenzies - implement hold function

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -11,8 +11,8 @@ export default function App() {
       .fill(0)
       .map(() => ({
         value: Math.ceil(Math.random() * 6),
-        isHeld: true,
-
+        isHeld: false,
+        id: nanoid()
       }))
   }
 
@@ -20,11 +20,20 @@ export default function App() {
     setDice(generateAllNewDice())
   }
 
+  function hold(id) {
+    setDice(oldDice => oldDice.map(die => 
+      die.id === id ?
+        {...die, isHeld: !die.isHeld} :
+        die
+    ))
+  }
+
   const diceElements = dice.map(dieObj => (
     <Die 
       key={dieObj.id} 
       value={dieObj.value}
       isHeld={dieObj.isHeld}
+      hold={() => hold(dieObj.id)}
     />
   ))
 
--- tenzies/src/components/Die.jsx
+++ tenzies/src/components/Die.jsx
@@ -3,6 +3,6 @@ export default function App(props) {
         backgroundColor: props.isHeld ? "#59E391" : "white"
     }
     return (
-        <button style={styles}>{props.value}</button>
+        <button onClick={props.hold} style={styles}>{props.value}</button>
     )
 }
