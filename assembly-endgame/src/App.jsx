48b64f77116fb98dc7c2f944a4dc9aecda0b01d5
Neil <iccir919@gmail.com>
2025-07-16 07:31:45 -0700

    assembly-endgame - add header

--- assembly-endgame/src/App.jsx
+++ assembly-endgame/src/App.jsx
@@ -3,7 +3,10 @@ import { useState } from 'react'
 function App() {
   return (
     <main>
-      Game goes here
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+      </header>
     </main>
   )
 }
