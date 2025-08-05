c4020d6eeae0b9f8b8918a302df378c2e7438b89
Neil <iccir919@gmail.com>
2025-08-05 10:15:06 -0700

    quizzical - implement fetching questions from API

--- quizzical/src/App.jsx
+++ quizzical/src/App.jsx
@@ -7,11 +7,23 @@ function App() {
   const [showQuiz, setShowQuiz] = useState(false)
   const [questions, setQuestions] = useState([])
 
-
-
   function startQuiz() {
     setShowQuiz(true)
   }
+
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=5")
+      .then(res => {
+        if (!res.ok) throw Error("Something went wrong")
+        return res.json()
+      })
+      .then(data => {
+        console.log(data.results)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }, [])
 
   return (
     <main>
