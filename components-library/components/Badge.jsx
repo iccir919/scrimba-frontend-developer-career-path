5060b0e832de2838fa6acde6acaaa6d58a3aaccf
Neil <iccir919@gmail.com>
2025-08-24 22:39:57 -0700

    component-library - complete badge

---
+++ components-library/components/Badge.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import classNames from "classnames"
+
+export default function Badge({ 
+    children, 
+    color = "grey",
+    type = "square"
+}) {
+
+    const componentClassNames = classNames("badge", {
+        "pill": type === "pill",
+        "square": type === "square",
+        "grey": color === "grey",
+        "red": color === "red",
+        "yellow": color === "yellow",
+        "green": color === "green",
+        "blue": color === "blue",
+        "purple": color === "purple",
+        "indigo": color === "indigo",
+        "pink": color === "pink"
+    })
+
+    return (
+        <div className={componentClassNames}>
+            <span>{children}</span>
+        </div>
+    )
+}
--- components-library/index.jsx
+++ components-library/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import Menu from "./components/Menu/index"
-import Star from "./components/Star"
+import Badge from "./components/Badge"
 
 import './style.css'
 
@@ -11,6 +11,8 @@ function App() {
     <div className="page-container">
       <h1>Component Library</h1>
       <p>This is a React component library and is a project for the Frontend Path from <a href="https://scrimba.com/">Scrimba</a>.</p>
+      
+
       <h2>Menu</h2>
       <Menu>
         <Menu.Button>Sports</Menu.Button>
@@ -21,6 +23,30 @@ function App() {
           <Menu.Item>Football</Menu.Item>
         </Menu.Dropdown>
       </Menu>
+      
+      <h2>Badge</h2>
+      <h3>Square</h3>
+      <div className="badges-row">
+        <Badge color="grey">Badge</Badge>
+        <Badge color="red">Badge</Badge>
+        <Badge color="yellow">Badge</Badge>
+        <Badge color="green">Badge</Badge>
+        <Badge color="blue">Badge</Badge>
+        <Badge color="indigo">Badge</Badge>
+        <Badge color="purple">Badge</Badge>
+        <Badge color="pink">Badge</Badge>
+      </div>
+      <h3>Pill</h3>
+      <div className="badges-row">
+        <Badge color="grey" type="pill">Badge</Badge>
+        <Badge color="red" type="pill">Badge</Badge>
+        <Badge color="yellow" type="pill">Badge</Badge>
+        <Badge color="green" type="pill">Badge</Badge>
+        <Badge color="blue" type="pill">Badge</Badge>
+        <Badge color="indigo" type="pill">Badge</Badge>
+        <Badge color="purple" type="pill">Badge</Badge>
+        <Badge color="pink" type="pill">Badge</Badge>
+      </div>
     </div>
   )
 }
