74639a1d11e55d6c0bc84f46ed4b7652bb3b6094
Neil <iccir919@gmail.com>
2025-09-02 14:10:53 -0700

    components-library - production build

--- components-library/vite.config.js
+++ components-library/vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
 	plugins: [
 		react()
-	]
+	],
+	base: 'https://neil-scrimba-frontend-career-path.netlify.app/components-library/dist/'
 })
